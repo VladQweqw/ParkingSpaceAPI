c6c6641f0a6288f7140fdb0827c0bcb9db5926fb
Vlad <vladpoienariu@gmail.com>
2025-02-15 15:15:01 +0200

    first commit

---
+++ src/test/java/com/example/parking_space_api/ParkingSpaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parking_space_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
